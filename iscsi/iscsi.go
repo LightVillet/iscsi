e759e21a21d728e46f0ff88f8dd46c2d62f13010
LightVillet <maxim12000@ya.ru>
2022-05-09 16:22:53 +0300

    Added discovery mode, support for vital pages in inquiry

--- iscsi/iscsi.go
+++ iscsi/iscsi.go
@@ -17,32 +17,55 @@ const (
 )
 
 // https://datatracker.ietf.org/doc/html/rfc3720#section-10.2.1.2
+type Opcode byte
+
 const (
 	// Initiator opcodes
-	NOP_OUT_OPCODE       = 0x00
-	SCSI_COMMAND_OPCODE  = 0x01
-	LOGIN_REQ_OPCODE     = 0x03
-	SCSI_DATA_OUT_OPCODE = 0x05
+	NOP_OUT_OPCODE       Opcode = 0x00
+	SCSI_COMMAND_OPCODE         = 0x01
+	LOGIN_REQ_OPCODE            = 0x03
+	TEXT_REQ_OPCODE             = 0x04
+	SCSI_DATA_OUT_OPCODE        = 0x05
 	// Target opcodes
 	NOP_IN_OPCODE       = 0x20
 	SCSI_RESP_OPCODE    = 0x21
 	LOGIN_RESP_OPCODE   = 0x23
+	TEXT_RESP_OPCODE    = 0x24
 	SCSI_DATA_IN_OPCODE = 0x25
 	REJECT_OPCODE       = 0x3f
 )
 
 // https://datatracker.ietf.org/doc/html/rfc3720#section-7.1.1
+type State byte
+
 const (
-	STATE_FREE      = 1
-	STATE_LOGGED_IN = 5
+	STATE_FREE      State = 1
+	STATE_LOGGED_IN       = 5
 )
 
 // https://datatracker.ietf.org/doc/html/rfc3720#section-10.12.3
+type Stage byte
+
 const (
-	STAGE_SEC_NEG      = 0
-	STAGE_LOGIN_OP_NEG = 1
-	STAGE_FULL         = 3
+	STAGE_SEC_NEG      Stage = 0
+	STAGE_LOGIN_OP_NEG       = 1
+	STAGE_FULL               = 3
 )
+
+// Target's constants
+const (
+	discoveryData = "TargetName=test.abc\x00TargetAddress=127.0.0.1:3260,1\x00"
+)
+
+// Device constants
+const (
+	DEVICE_PAGE_LEN = 32 // Device Identification Page - IBM Bridged
+)
+
+var VITAL_PAGES = [...]byte{0x00, 0x80, 0x83}//, 0xb1, 0xb2, 0xb0}
+
+// https://docs.oracle.com/en/storage/tape-storage/storagetek-sl150-modular-tape-library/slorm/report-luns-a0h.html
+var LUNS = [...]uint64{0x00, 0x01}
 
 type Config struct {
 	Host string
@@ -56,7 +79,7 @@ type Server struct {
 
 type BHS struct {
 	immediate         bool
-	opcode            byte
+	opcode            Opcode
 	final             bool
 	arg0              []byte
 	totalAHSLength    byte
@@ -75,19 +98,17 @@ type Session struct {
 	conn             net.Conn
 	maxRecvDSL       int
 	initiatorTaskTag uint32
-	status           int
+	status           State
+	// https://datatracker.ietf.org/doc/html/rfc3720#section-3.3
+	isDiscoverySession bool
 }
 
 type CDB struct {
 	cdb_length          byte
 	groupCode           byte
-	commandCode         byte
-	arg0                byte
-	logicalBlockAddress uint64
-	transferLength      byte
-	paramLIstLength     byte
-	allocationLength    uint16
-	arg1                byte
+	opCode              byte
+	arg                 []byte
+	allocationLength    uint32
 	control             byte
 }
 
@@ -141,7 +162,7 @@ func (s *Session) readGor() {
 		s.initiatorTaskTag = p.bhs.initiatorTaskTag
 		dataLen := p.bhs.dataSegmentLength
 		// Including padding
-		if dataLen/4 != 0 {
+		if dataLen%4 != 0 {
 			dataLen += 4 - dataLen%4
 		}
 		p.data = make([]byte, dataLen)
@@ -155,10 +176,15 @@ func (s *Session) readGor() {
 			err = s.handleNOPReq(p)
 		case SCSI_COMMAND_OPCODE:
 			err = s.handleSCSIReq(p)
+		case TEXT_REQ_OPCODE:
+			err = s.hangleTextReq(p)
 		case LOGIN_REQ_OPCODE:
 			err = s.handleLoginReq(p)
 		default:
-			fmt.Printf("Unsopported opcode: %x\n", p.bhs.opcode)
+			if s.isDiscoverySession {
+				fmt.Printf("In discovery session:\n")
+			}
+			fmt.Printf("Unsupported opcode: %x\n", p.bhs.opcode)
 			err = s.handleUnsupportedReq(p)
 		}
 		if err != nil {
@@ -176,7 +202,7 @@ func (p *ISCSIPacket) recvBHS(conn net.Conn) error {
 	if (buf[0]&IMMEDIATE_DELIVERY_BITMASK)<<7 == 1 {
 		p.bhs.immediate = true
 	}
-	p.bhs.opcode = buf[0] &^ IMMEDIATE_DELIVERY_BITMASK
+	p.bhs.opcode = (Opcode)(buf[0] &^ IMMEDIATE_DELIVERY_BITMASK)
 	if (buf[1]&FINAL_PDU_BITMASK)>>7 == 1 {
 		p.bhs.final = true
 	}
@@ -206,6 +232,36 @@ func (s *Session) handleNOPReq(req ISCSIPacket) error {
 	return s.send(ans)
 }
 
+func (s *Session) hangleTextReq(req ISCSIPacket) error {
+	if !s.isDiscoverySession {
+		return errors.New("Unssuported text request in not discovery session\n")
+	}
+	ans := ISCSIPacket{}
+	ans.bhs.arg0 = make([]byte, 3)
+	lenData := len(discoveryData)
+	if (lenData%4 != 0) {
+		lenData += 4 - lenData%4
+	}
+	ans.data = make([]byte, lenData)
+	ans.bhs.opcode = TEXT_RESP_OPCODE
+	ans.bhs.final = true
+	ans.bhs.dataSegmentLength = (uint32)(lenData)
+	ans.bhs.arg1 = make([]byte, 8)
+	ans.bhs.initiatorTaskTag = req.bhs.initiatorTaskTag
+	ans.bhs.arg2 = make([]byte, 28)
+	binary.BigEndian.PutUint32(ans.bhs.arg2[0:4], 0xFFFFFFFF)
+	// StatSN
+	binary.LittleEndian.PutUint32(ans.bhs.arg2[4:8], req.bhs.initiatorTaskTag)
+	// ExpCmdSN
+	binary.LittleEndian.PutUint32(ans.bhs.arg2[8:12], req.bhs.initiatorTaskTag)
+	// MaxCmdSN
+	binary.LittleEndian.PutUint32(ans.bhs.arg2[12:16], req.bhs.initiatorTaskTag)
+	ans.data = make([]byte, lenData)
+	copy(ans.data, discoveryData)
+	err := s.send(ans)
+	return err
+}
+
 func (s *Session) handleLoginReq(req ISCSIPacket) error {
 	args := make(map[string]string)
 	var err error
@@ -217,6 +273,9 @@ func (s *Session) handleLoginReq(req ISCSIPacket) error {
 		}
 	}
 	s.maxRecvDSL, err = strconv.Atoi(args["MaxRecvDataSegmentLength"])
+	if args["SessionType"] == "Discovery" {
+		s.isDiscoverySession = true
+	}
 	if err != nil {
 		return err
 	}
@@ -240,40 +299,13 @@ func (s *Session) handleLoginReq(req ISCSIPacket) error {
 }
 
 func (s *Session) handleSCSIReq(req ISCSIPacket) error {
-	ans := ISCSIPacket{}
 	var err error
-	cdb := CDB{}
-	cdb.groupCode = req.bhs.arg2[12] >> 5
-	cdb.commandCode = req.bhs.arg2[12] & 0x1F
-	// Group code determines cdb length
-	switch cdb.groupCode {
-	case 0b000:
-		cdb.cdb_length = 6
-	default:
-		return errors.New(fmt.Sprintf("Error parsing CDB: unsopported group code %x", cdb.groupCode))
-	}
-	cdb.control = req.bhs.arg2[12+cdb.cdb_length-1]
-	switch cdb.commandCode {
-	case 0x12:
-		ans, err = cdb.parseInquiryCDB(req)
-	default:
-		return errors.New(fmt.Sprintf("Error parsing CDB: unsopported command code %x", cdb.commandCode))
-	}
-	err = s.send(ans)
-	return err
-}
-
-// https://docs.oracle.com/en/storage/tape-storage/storagetek-sl150-modular-tape-library/slorm/inquiry-12h.html
-func (cdb *CDB) parseInquiryCDB(req ISCSIPacket) (ISCSIPacket, error) {
-	cdb.allocationLength = binary.BigEndian.Uint16(req.bhs.arg2[15:17])
-	// Temporary sending no devices
-	// TODO
 	ans := ISCSIPacket{}
+	cdb := CDB{}
 	ans.bhs.opcode = SCSI_DATA_IN_OPCODE
 	ans.bhs.final = true
 	ans.bhs.arg0 = make([]byte, 3)
 	ans.bhs.arg0[0] = 1
-	ans.bhs.dataSegmentLength = uint32(cdb.allocationLength)
 	ans.bhs.initiatorTaskTag = req.bhs.initiatorTaskTag
 	ans.bhs.arg2 = make([]byte, 28)
 	// Target transfer tag
@@ -282,14 +314,131 @@ func (cdb *CDB) parseInquiryCDB(req ISCSIPacket) (ISCSIPacket, error) {
 	binary.LittleEndian.PutUint32(ans.bhs.arg2[4:8], req.bhs.initiatorTaskTag)
 	// ExpCmdSN
 	binary.LittleEndian.PutUint32(ans.bhs.arg2[8:12], req.bhs.initiatorTaskTag)
-	// MaxCmdSN ?????
-	binary.LittleEndian.PutUint32(ans.bhs.arg2[12:16], req.bhs.initiatorTaskTag)
-	ans.data = make([]byte, int(ans.bhs.dataSegmentLength))
-	// Peripheral qualifier + device type
-	ans.data[0] = (0b011 << 5) + 0x1F
-	// Responce data format
-	ans.data[3] = 2
-	return ans, nil
+	// MaxCmdSN
+	binary.LittleEndian.PutUint32(ans.bhs.arg2[12:16], req.bhs.initiatorTaskTag + 8)
+	cdb.groupCode = req.bhs.arg2[12] >> 5
+	cdb.opCode = req.bhs.arg2[12]
+	// Group code determines cdb length
+	switch cdb.groupCode {
+	case 0b000:
+		cdb.cdb_length = 6
+		cdb.arg = make([]byte, 3)
+		copy(cdb.arg, req.bhs.arg2[13:16])
+		cdb.allocationLength = uint32(req.bhs.arg2[16])
+	case 0b101:
+		cdb.cdb_length = 12
+		cdb.arg = make([]byte, 4)
+		copy(cdb.arg, req.bhs.arg2[14:18])
+		cdb.allocationLength = binary.BigEndian.Uint32(req.bhs.arg2[18:22])
+	default:
+		return errors.New(fmt.Sprintf("Error parsing CDB: unsupported group code %x\n", cdb.groupCode))
+	}
+	cdb.control = req.bhs.arg2[12+cdb.cdb_length-1]
+	switch cdb.opCode {
+	case 0x12:
+		ans.data, err = cdb.parseInquiryCDB()
+	case 0xA0:
+		ans.data, err = cdb.parseReportLunCDB()
+	default:
+		err = errors.New(fmt.Sprintf("Error parsing CDB: unsupported command code %x\n", cdb.opCode))
+	}
+	if err != nil {
+		return err
+	}
+	// Truncating if needed
+	if int(cdb.allocationLength) < len(ans.data) {
+		ans.bhs.dataSegmentLength = cdb.allocationLength
+	} else {
+		ans.bhs.dataSegmentLength = uint32(len(ans.data))
+	}
+	err = s.send(ans)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// https://docs.oracle.com/en/storage/tape-storage/storagetek-sl150-modular-tape-library/slorm/report-luns-a0h.html
+func (cdb *CDB) parseReportLunCDB() ([]byte, error) {
+	var data []byte
+	data = make([]byte, 8 + len(LUNS)*8)
+	// LUN list length
+	binary.BigEndian.PutUint32(data[0:4], uint32(len(LUNS)*8))
+	for i, Lun := range LUNS {
+		binary.LittleEndian.PutUint64(data[8*(i+1):8*(i+2)], Lun)
+	}
+	return data, nil
+}
+
+// https://docs.oracle.com/en/storage/tape-storage/storagetek-sl150-modular-tape-library/slorm/inquiry-12h.html
+func (cdb *CDB) parseInquiryCDB() ([]byte, error) {
+	var data []byte
+	if cdb.arg[0] == 0 { // Product data
+		data = make([]byte, DEVICE_PAGE_LEN)
+		// Peripheral qualifier + device type
+		data[0] = (0b000 << 5) + 0x0C
+		// Version
+		data[2] = 0x05
+		// Response data format
+		data[3] = 2
+		// Additional length
+		data[4] = 62
+		// Vendor ID
+		copy(data[8:16], "COOLCMPY")
+		// Product ID
+		copy(data[16:27], "COOLDEVICE")
+	} else { // Vital Product Data
+		switch cdb.arg[1] { // Type of vital page
+		case 0x00: // List of vital pages
+			data = make([]byte, 4 + len(VITAL_PAGES))
+			for i, vitalPage := range VITAL_PAGES {
+				data[4 + i] = vitalPage
+			}
+		case 0x80: // Unit serial number page
+			data = make([]byte, 8)
+			data[3] = 4
+			copy(data[4:8], "COOL")
+		case 0x83: // Device Identification
+			data = make([]byte, 48)
+			// Protocol id | code set
+			//data[4] = 2
+			data[4] = (0x6 << 4) + 0x2
+			// PIV | Association | id type
+			//data[5] = 1
+			data[5] = (0b1 << 7) + 0x1
+			// id length
+			data[7] = 8
+			copy(data[8:16], "VERYCOOL")
+			// https://github.com/fujita/tgt/blob/master/usr/spc.c#L162
+			data[16] = 1
+			data[17] = 3
+			data[19] = 8
+			binary.BigEndian.PutUint64(data[20:28], LUNS[0])
+			data[20] |= 3 << 4
+			data[28] = 1
+			data[29] = 3
+			data[31] = 0x10
+			// NAA_DESG_LEN_EXTD
+			binary.BigEndian.PutUint64(data[32:40], LUNS[0])
+			binary.BigEndian.PutUint64(data[40:48], LUNS[0])
+			data[32] &= 0x0F
+			data[32] |= 0x6 << 4
+		case 0xb0:
+			data = make([]byte, 12)
+			// Maximum compare and write length
+			data[5] = 0
+			// Maximum transfer length
+			binary.BigEndian.PutUint32(data[8:12], 0xFFFFFFFF)
+		default:
+			return nil, errors.New(fmt.Sprintf("Error parsing CDB: unsupported vital product page: %x\n", cdb.arg[1]))
+		}
+		data[0] = (0b000 << 5) + 0x0C
+		data[1] = cdb.arg[1]
+		// Data length in 2 bytes
+		data[2] = byte((len(data) - 4) >> 8)
+		data[3] = byte(len(data) - 4)
+	}
+	return data, nil
 }
 
 func (s *Session) handleUnsupportedReq(req ISCSIPacket) error {
@@ -303,7 +452,7 @@ func (s *Session) handleUnsupportedReq(req ISCSIPacket) error {
 	copy(ans.bhs.arg1, req.bhs.arg1)
 	ans.bhs.initiatorTaskTag = 0xFFFFFFFF
 	ans.data = make([]byte, 48+req.bhs.dataSegmentLength)
-	ans.data[0] = req.bhs.opcode
+	ans.data[0] = (byte)(req.bhs.opcode)
 	if req.bhs.immediate {
 		ans.data[0] |= 0b01000000
 	}
@@ -322,7 +471,7 @@ func (s *Session) handleUnsupportedReq(req ISCSIPacket) error {
 
 func (s *Session) send(p ISCSIPacket) error {
 	buf := make([]byte, 48)
-	buf[0] = p.bhs.opcode
+	buf[0] = (byte)(p.bhs.opcode)
 	if p.bhs.immediate {
 		buf[0] |= 0b01000000
 	}
@@ -340,14 +489,20 @@ func (s *Session) send(p ISCSIPacket) error {
 		return err
 	}
 	if uint32(sendLen) != 48 {
-		return errors.New(fmt.Sprintf("Error sending BHS: expected %d bytes, sent %d bytes", 48, sendLen))
+		return errors.New(fmt.Sprintf("Error sending BHS: expected %d bytes, sent %d bytes\n", 48, sendLen))
 	}
-	sendLen, err = s.conn.Write(p.data)
+	// Padding
+	if p.bhs.dataSegmentLength%4 != 0 {
+		pad := make([]byte, 4 - p.bhs.dataSegmentLength%4)
+		p.data = append(p.data, pad...)
+		p.bhs.dataSegmentLength += 4 - p.bhs.dataSegmentLength%4
+	}
+	sendLen, err = s.conn.Write(p.data[0:p.bhs.dataSegmentLength])
 	if err != nil {
 		return err
 	}
 	if uint32(sendLen) != p.bhs.dataSegmentLength {
-		return errors.New(fmt.Sprintf("Error sending data: expected %d bytes, sent %d bytes", p.bhs.dataSegmentLength, sendLen))
+		return errors.New(fmt.Sprintf("Error sending data: expected %d bytes, sent %d bytes\n", p.bhs.dataSegmentLength, sendLen))
 	}
 	return nil
 }
